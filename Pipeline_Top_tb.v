5cca7ba94c732c5b30e0a233a8d765f49fc21311
ankit02601 <147239507+ankit02601@users.noreply.github.com>
2025-07-21 19:08:28 +0530

    Create Pipeline_Top_tb.v

---
+++ Pipeline_Top_tb.v
@@ -0,0 +1,23 @@
+
+
+module Pipeline_Top_tb();
+
+reg clk=1'b0,rst;
+
+
+Pipeline_Top dut(.clk(clk),.rst(rst));
+
+always begin
+clk=~clk;
+#50;
+end
+
+initial begin
+rst<=1'b0;
+#200;
+rst<=1'b1;
+#1000;
+$finish;
+end
+    
+endmodule
